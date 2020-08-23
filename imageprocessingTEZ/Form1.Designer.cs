da7d7bfac1b70356bcf4c5f9e8d3636e4741bbc2
sinansa91 <sinanomik@gmail.com>
2020-08-24 01:49:49 +0300

    First Commit

---
+++ imageprocessingTEZ/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace imageprocessingTEZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.açToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arkaplanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resimİşlemeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayScaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.histogramYapmaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageSubstractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otsuÇıkarımToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.morphologyBoşlukDoldurmaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.resimİşlemeToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            resources.ApplyResources(this.dosyaToolStripMenuItem, "dosyaToolStripMenuItem");
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.açToolStripMenuItem,
+            this.arkaplanToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            // 
+            // açToolStripMenuItem
+            // 
+            resources.ApplyResources(this.açToolStripMenuItem, "açToolStripMenuItem");
+            this.açToolStripMenuItem.Name = "açToolStripMenuItem";
+            this.açToolStripMenuItem.Click += new System.EventHandler(this.açToolStripMenuItem_Click);
+            // 
+            // arkaplanToolStripMenuItem
+            // 
+            resources.ApplyResources(this.arkaplanToolStripMenuItem, "arkaplanToolStripMenuItem");
+            this.arkaplanToolStripMenuItem.Name = "arkaplanToolStripMenuItem";
+            this.arkaplanToolStripMenuItem.Click += new System.EventHandler(this.arkaplanToolStripMenuItem_Click);
+            // 
+            // resimİşlemeToolStripMenuItem
+            // 
+            resources.ApplyResources(this.resimİşlemeToolStripMenuItem, "resimİşlemeToolStripMenuItem");
+            this.resimİşlemeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grayScaleToolStripMenuItem,
+            this.histogramYapmaToolStripMenuItem,
+            this.imageSubstractToolStripMenuItem,
+            this.otsuÇıkarımToolStripMenuItem,
+            this.morphologyBoşlukDoldurmaToolStripMenuItem});
+            this.resimİşlemeToolStripMenuItem.Name = "resimİşlemeToolStripMenuItem";
+            this.resimİşlemeToolStripMenuItem.Click += new System.EventHandler(this.resimİşlemeToolStripMenuItem_Click);
+            // 
+            // grayScaleToolStripMenuItem
+            // 
+            resources.ApplyResources(this.grayScaleToolStripMenuItem, "grayScaleToolStripMenuItem");
+            this.grayScaleToolStripMenuItem.Name = "grayScaleToolStripMenuItem";
+            this.grayScaleToolStripMenuItem.Click += new System.EventHandler(this.grayScaleToolStripMenuItem_Click);
+            // 
+            // histogramYapmaToolStripMenuItem
+            // 
+            resources.ApplyResources(this.histogramYapmaToolStripMenuItem, "histogramYapmaToolStripMenuItem");
+            this.histogramYapmaToolStripMenuItem.Name = "histogramYapmaToolStripMenuItem";
+            this.histogramYapmaToolStripMenuItem.Click += new System.EventHandler(this.histogramYapmaToolStripMenuItem_Click);
+            // 
+            // imageSubstractToolStripMenuItem
+            // 
+            resources.ApplyResources(this.imageSubstractToolStripMenuItem, "imageSubstractToolStripMenuItem");
+            this.imageSubstractToolStripMenuItem.Name = "imageSubstractToolStripMenuItem";
+            this.imageSubstractToolStripMenuItem.Click += new System.EventHandler(this.imageSubstractToolStripMenuItem_Click);
+            // 
+            // otsuÇıkarımToolStripMenuItem
+            // 
+            resources.ApplyResources(this.otsuÇıkarımToolStripMenuItem, "otsuÇıkarımToolStripMenuItem");
+            this.otsuÇıkarımToolStripMenuItem.Name = "otsuÇıkarımToolStripMenuItem";
+            this.otsuÇıkarımToolStripMenuItem.Click += new System.EventHandler(this.otsuÇıkarımToolStripMenuItem_Click);
+            // 
+            // morphologyBoşlukDoldurmaToolStripMenuItem
+            // 
+            resources.ApplyResources(this.morphologyBoşlukDoldurmaToolStripMenuItem, "morphologyBoşlukDoldurmaToolStripMenuItem");
+            this.morphologyBoşlukDoldurmaToolStripMenuItem.Name = "morphologyBoşlukDoldurmaToolStripMenuItem";
+            this.morphologyBoşlukDoldurmaToolStripMenuItem.Click += new System.EventHandler(this.morphologyBoşlukDoldurmaToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+            // 
+            // tabControl1
+            // 
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // tabPage1
+            // 
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox6, 2, 1);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            resources.ApplyResources(this.pictureBox4, "pictureBox4");
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox4_MouseDown);
+            this.pictureBox4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox4_MouseMove);
+            this.pictureBox4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox4_MouseUp);
+            // 
+            // pictureBox5
+            // 
+            resources.ApplyResources(this.pictureBox5, "pictureBox5");
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            resources.ApplyResources(this.pictureBox6, "pictureBox6");
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Controls.Add(this.pictureBox9);
+            this.tabPage2.Controls.Add(this.pictureBox8);
+            this.tabPage2.Controls.Add(this.pictureBox7);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox9
+            // 
+            resources.ApplyResources(this.pictureBox9, "pictureBox9");
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            resources.ApplyResources(this.pictureBox8, "pictureBox8");
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            resources.ApplyResources(this.pictureBox7, "pictureBox7");
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.TabStop = false;
+            // 
+            // tabPage3
+            // 
+            resources.ApplyResources(this.tabPage3, "tabPage3");
+            this.tabPage3.Controls.Add(this.pictureBox10);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox10
+            // 
+            resources.ApplyResources(this.pictureBox10, "pictureBox10");
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox10_MouseDown);
+            this.pictureBox10.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox10_MouseMove);
+            this.pictureBox10.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox10_MouseUp);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            resources.ApplyResources(this.openFileDialog2, "openFileDialog2");
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem açToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem resimİşlemeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayScaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem histogramYapmaToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.ToolStripMenuItem arkaplanToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.ToolStripMenuItem imageSubstractToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.ToolStripMenuItem otsuÇıkarımToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.ToolStripMenuItem morphologyBoşlukDoldurmaToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox pictureBox10;
+    }
+}
+
---
+++ imageprocessingTEZ/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.ColorReduction;
+using AForge.Imaging.ComplexFilters;
+using AForge.Imaging.Filters;
+
+namespace imageprocessingTEZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Drawing.Graphics formGraphics;
+        bool isDown = false;
+        int initialX;
+        int initialY;
+        private void açToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            
+            if (openFileDialog1.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Image = (Bitmap)System.Drawing.Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void grayScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GrayscaleBT709 gray= new GrayscaleBT709();
+            pictureBox2.Image=gray.Apply((Bitmap)pictureBox1.Image);
+            pictureBox5.Image = gray.Apply((Bitmap)pictureBox4.Image);
+
+
+        }
+
+        private void histogramYapmaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HistogramEqualization histogram = new HistogramEqualization();
+            pictureBox3.Image = histogram.Apply((Bitmap)pictureBox2.Image);
+            pictureBox6.Image = histogram.Apply((Bitmap)pictureBox5.Image);
+        }
+
+        private void resimİşlemeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void arkaplanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox4.Image = (Bitmap)System.Drawing.Image.FromFile(openFileDialog2.FileName);
+                pictureBox10.Image = (Bitmap)System.Drawing.Image.FromFile(openFileDialog2.FileName);
+            }
+        }
+
+        private void imageSubstractToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // filtre oluşturduk
+            Difference filter = new Difference((Bitmap)pictureBox6.Image);
+            // uyguladık
+            Bitmap cikarim = filter.Apply((Bitmap)pictureBox3.Image);
+            pictureBox7.Image = cikarim;
+
+
+            
+
+                       
+
+        }
+
+        private void otsuÇıkarımToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Threshold filter = new Threshold(100);
+            // apply the filter
+           Bitmap otsu =   filter.Apply((Bitmap)pictureBox7.Image);
+           pictureBox8.Image = otsu;
+        }
+
+        private void morphologyBoşlukDoldurmaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // create filter
+            Closing filter = new Closing();
+            // apply the filter
+            Bitmap morf = filter.Apply((Bitmap)pictureBox8.Image);
+            pictureBox9.Image = morf;
+        }
+
+        private void pictureBox4_MouseDown(object sender, MouseEventArgs e)
+        
+        {
+            
+           
+        }
+
+        private void pictureBox4_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void pictureBox4_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            
+        }
+
+        private void pictureBox10_MouseUp(object sender, MouseEventArgs e)
+        {
+            String strpos = "X kordinatı " + initialX.ToString() + " Y kordinatı " + initialY.ToString();
+            isDown = false;
+            MessageBox.Show(strpos, "Yeni Park Alanı Kaydedildi.", MessageBoxButtons.OK);
+        }
+
+        private void pictureBox10_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDown == true)
+            {
+                this.Refresh();
+                Brush Green = new SolidBrush(Color.Green);
+                Pen drwaPen = new Pen(Green, 7);
+                int width = e.X - initialX, height = e.Y - initialY;
+                //if (Math.Sign (width) == -1) width = width 
+                //Rectangle rect = new Rectangle(initialPt.X, initialPt.Y, Cursor.Position.X - initialPt.X, Cursor.Position.Y - initialPt.Y);
+                Rectangle rect = new Rectangle(initialX, initialY, width * Math.Sign(width), height * Math.Sign(height));
+
+                Graphics graf = pictureBox10.CreateGraphics();
+
+
+
+
+                graf.DrawRectangle(drwaPen, rect);
+            }
+        }
+
+        private void pictureBox10_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDown = true;
+            initialX = e.X;
+            initialY = e.Y;
+        }
+    }
+}
